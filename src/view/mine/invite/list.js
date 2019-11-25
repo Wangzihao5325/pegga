d857cccefe5d7a4e54c4ae11e43ac3cae8397dcb
Wangzihao5325 <416990273@qq.com>
2019-11-25 09:11:20 +0800

    1.嵌入邀请页面（暂无入口）

---
+++ src/view/mine/invite/list.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import {
+    SafeAreaView,
+    Dimensions,
+    StyleSheet,
+} from 'react-native';
+import { WebView } from 'react-native-webview';
+import Header from '../../../component/header';
+import Variables from '../../../global/Variables';
+
+export default class InviteList extends Component {
+    static navigationOptions = ({ navigation }) => {
+        return {
+            header: null,
+            headerBackTitle: null
+        }
+    };
+
+    render() {
+        let uri = `http://192.168.3.127:8080/#/list?t=${Variables.account.token}`
+        return (
+            <SafeAreaView style={styles.safeContainer}>
+                <Header.Normal
+                    title='下级列表'
+                    goback={() => this.props.navigation.goBack()}
+                />
+                <WebView
+                    source={{ uri }}
+                    style={{ width: Dimensions.get('window').width }}
+                />
+            </SafeAreaView>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    safeContainer: {
+        flex: 1,
+        display: 'flex',
+        backgroundColor: 'white',
+        flexDirection: 'column',
+        alignItems: 'center'
+    }
+});
