ce1a18fe1278ba02384de4fb5b641112db69056a
Wangzihao5325 <416990273@qq.com>
2019-12-09 13:47:21 +0800

    1.处理无法进行测试转账的问题

--- src/global/Config.js
+++ src/global/Config.js
@@ -1,7 +1,7 @@
 const SERVICE_URL = {
     domain: 'http://ec2-54-254-247-159.ap-southeast-1.compute.amazonaws.com:4000',
     imageDomain: 'http://ec2-54-254-247-159.ap-southeast-1.compute.amazonaws.com:4000/api/file/upload/image',
-    payment: 'http://ec2-54-255-146-127.ap-southeast-1.compute.amazonaws.com:8080',
+    payment: 'http://ec2-54-255-146-127.ap-southeast-1.compute.amazonaws.com:8080/ali.html',
     invite: 'http://ec2-54-255-146-127.ap-southeast-1.compute.amazonaws.com:8081/#/'
 };
 const INFO = { iosVer: '1.0.0', androidVer: '1.0.0' };
