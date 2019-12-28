e7d67a3e2e964d6060c7056890529035c17b48cb
Wangzihao5325 <416990273@qq.com>
2019-12-28 13:55:13 +0800

    1.资金流水优化

--- src/view/assets/moneyFlow/billTypeSelectModel/index.js
+++ src/view/assets/moneyFlow/billTypeSelectModel/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import {
+    Animated,
     SafeAreaView,
     TouchableWithoutFeedback,
     TouchableHighlight,
@@ -9,7 +10,7 @@ import {
     Platform,
     StyleSheet
 } from 'react-native';
-
+import Btn from '../../../..//component/btn';
 import Enum from '../../../../global/Enum';
 import store from '../../../../store';
 import { assets_update_bill_type, assets_update_bill_time_type } from '../../../../store/actions/assetsAction';
@@ -30,25 +31,49 @@ function Item(props) {
 export default class BillTypeSelectModel extends Component {
 
     state = {
+        fadeAnim: new Animated.Value(0),
         data: [],
-        type: ''
+        type: '',
+        title: '',
     }
 
     componentDidMount() {
+        this.timer = setTimeout(() => {
+            this.timer = null
+            Animated.timing(
+                this.state.fadeAnim,
+                {
+                    toValue: 1,
+                }
+            ).start();
+
+        }, 300);
+
         const type = this.props.navigation.getParam('type', '');
         if (type) {
             this.setState({
                 data: Enum.ASSETS[type],
-                type
+                type,
+                title: '请选择'
             });
+        }
+    }
+
+    componentWillUnmount() {
+        if (this.timer) {
+            clearTimeout(this.timer);
+            this.timer = null
         }
     }
 
     render() {
         return (
-            <TouchableWithoutFeedback style={styles.touchable} onPress={() => this.props.navigation.goBack()}>
-                <SafeAreaView style={styles.safeArea}>
-                    <View style={styles.container}>
+            <Animated.View style={{ flex: 1, opacity: this.state.fadeAnim }}>
+                <View style={styles.container}>
+                    <View>
+                        <View style={styles.titleContainer}>
+                            <Text style={styles.title}>{this.state.title}</Text>
+                        </View>
                         {
                             this.state.data.map((item, index) => {
                                 return (
@@ -58,14 +83,54 @@ export default class BillTypeSelectModel extends Component {
                                         callback={() => this.itemPress(item)}
                                         last={this.state.data.length - 1 === index}
                                     />
-                                )
+                                );
                             })
                         }
+                        <Btn.Linear
+                            style={styles.backBtn}
+                            textStyle={styles.backBtnText}
+                            btnPress={() => this.back(500)}
+                            title='确定'
+                        />
                     </View>
-                </SafeAreaView>
-            </TouchableWithoutFeedback>
+                </View>
+            </Animated.View>
+        )
+        // return (
+        //     <TouchableWithoutFeedback style={styles.touchable} onPress={() => this.props.navigation.goBack()}>
+        //         <SafeAreaView style={styles.safeArea}>
+        //             <View style={styles.container}>
+        //                 {
+        //                     this.state.data.map((item, index) => {
+        //                         return (
+        //                             <Item
+        //                                 key={item.key}
+        //                                 item={item}
+        //                                 callback={() => this.itemPress(item)}
+        //                                 last={this.state.data.length - 1 === index}
+        //                             />
+        //                         )
+        //                     })
+        //                 }
+        //             </View>
+        //         </SafeAreaView>
+        //     </TouchableWithoutFeedback>
 
-        );
+        // );
+    }
+
+    back = (value) => {
+        Animated.timing(
+            this.state.fadeAnim,
+            {
+                toValue: 0,
+                duration: 400
+            }
+        ).start();
+        this.timer = setTimeout(() => {
+            this.timer = null;
+            this.props.navigation.goBack();
+        }, value)
     }
 
     itemPress = (item) => {
@@ -79,40 +144,61 @@ export default class BillTypeSelectModel extends Component {
             default:
                 break;
         }
-        this.props.navigation.goBack();
+        this.back(500);
     }
 }
 
 const styles = StyleSheet.create({
-    safeArea: {
-        flex: 1,
-        backgroundColor: 'rgba(0,0,0,0)',
-        display: 'flex',
-        flexDirection: 'column-reverse',
-        alignItems: 'center'
-    },
-    touchable: {
-        flex: 1,
-    },
     container: {
-        borderRadius: 10,
-        marginBottom: 10,
-        ...Platform.select({
-            ios: {
-                shadowColor: 'black',
-                shadowOffset: { width: 2, height: 2 },
-                shadowOpacity: 0.2,
-            },
-            android: {
-                borderColor: 'rgb(222,222,222)',
-                borderWidth: 1
-            }
-        })
-
+        flex: 1,
+        backgroundColor: 'rgba(0,0,0,0.5)',
+        flexDirection: 'column',
+        justifyContent: 'flex-end'
+    },
+    itemContainer: {
+        height: 45,
+        width: Dimensions.get('window').width,
+        backgroundColor: 'white'
+    },
+    itemTextNormal: {
+        fontFamily: 'PingFang-SC-Regular',
+        fontSize: 16,
+        color: 'rgb(40,46,60)'
+    },
+    itemTextHighlight: {
+        fontFamily: 'PingFang-SC-Regular',
+        fontWeight: 'bold',
+        fontSize: 16,
+        color: 'rgb(64,99,213)'
+    },
+    titleContainer: {
+        height: 60,
+        width: Dimensions.get('window').width,
+        backgroundColor: 'white',
+        borderTopLeftRadius: 10,
+        borderTopRightRadius: 10
+    },
+    title: {
+        marginTop: 20,
+        alignSelf: 'center',
+        fontFamily: 'PingFang-SC-Regular',
+        fontWeight: 'bold',
+        fontSize: 16,
+        color: 'rgb(40,46,60)'
+    },
+    backBtn: {
+        height: 65,
+        width: Dimensions.get('window').width,
+    },
+    backBtnText: {
+        color: 'white',
+        textAlign: 'center',
+        textAlignVertical: 'center',
+        fontSize: 15
     },
     itemBtn: {
         height: 60,
-        width: Dimensions.get('window').width - 50,
+        width: Dimensions.get('window').width,
         backgroundColor: 'white'
     },
     itemTitle: {
